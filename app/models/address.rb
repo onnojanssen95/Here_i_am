44b4d40a982910bb37c97318a6b7e33c8745dc91
Gesa Asmus <gesa.asmus@me.com>
2022-06-01 11:51:22 +0200

    validation so far

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,25 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :address, presence: true
+  validates :kind_of_place, presence: true
+  validates :floor, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :elevator, exclusion: [nil]
 end
+
+
+# create_table "addresses", force: :cascade do |t|
+#   t.string "name"
+#   t.string "address"
+#   t.string "kind_of_place"
+#   t.string "place_in_building"
+#   t.integer "floor"
+#   t.boolean "elevator"
+#   t.text "description"
+#   t.bigint "user_id", null: false
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["user_id"], name: "index_addresses_on_user_id"
+# end
