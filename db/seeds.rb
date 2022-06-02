05d487d2cde34abfc81c5e3b5421a94673090190
Gesa Asmus <gesa.asmus@me.com>
2022-06-02 15:34:10 +0200

    map is working, pin correct, zooms in -> but 'add new address' on index page is not working and auto fill in on edit is not there

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,9 @@ n = 0
   puts user.email
   address = Address.new(
     name: "home#{n}",
-    address: " rudi geluksstrasse 27",
+    address: " Rudi-Dutschke-Straße 26",
     kind_of_place: "apartment",
-    place_in_building: " achterhuis",
+    place_in_building: "backside",
     floor: rand(1..10),
     elevator: false,
     description: "it is a super nice long path"
