0e73df45b77f330e59a76aef47ecb4ddcb11e7a1
onno janssen <onnojanssen@hotmail.com>
2022-06-07 16:59:27 +0200

    userinfo displayed on the profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-
   end
 
   def settings
-    
+    @user = current_user
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email)
+  end
+
 end
